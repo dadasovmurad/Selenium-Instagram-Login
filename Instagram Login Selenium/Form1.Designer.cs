4dbb2ca2aa1fc4b5c34e98ba497f63a286ba61ec
3lonAZ <66734133+3lonAZ@users.noreply.github.com>
2020-06-10 19:22:29 +0400

    Selenium Instagram Login

---
+++ Instagram Login Selenium/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Instagram_Login_Selenium
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.username_txtBox = new System.Windows.Forms.TextBox();
+            this.password_txtBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Login_BTN = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(140, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(165, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Aqua;
+            this.label1.Location = new System.Drawing.Point(11, 139);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Username";
+            // 
+            // username_txtBox
+            // 
+            this.username_txtBox.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.username_txtBox.Location = new System.Drawing.Point(121, 137);
+            this.username_txtBox.Name = "username_txtBox";
+            this.username_txtBox.Size = new System.Drawing.Size(301, 26);
+            this.username_txtBox.TabIndex = 2;
+            this.username_txtBox.TextChanged += new System.EventHandler(this.TestNullTxtBox);
+            // 
+            // password_txtBox
+            // 
+            this.password_txtBox.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.password_txtBox.Location = new System.Drawing.Point(121, 188);
+            this.password_txtBox.Name = "password_txtBox";
+            this.password_txtBox.Size = new System.Drawing.Size(301, 26);
+            this.password_txtBox.TabIndex = 4;
+            this.password_txtBox.UseSystemPasswordChar = true;
+            this.password_txtBox.TextChanged += new System.EventHandler(this.TestNullTxtBox);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.Aqua;
+            this.label2.Location = new System.Drawing.Point(11, 190);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password";
+            // 
+            // Login_BTN
+            // 
+            this.Login_BTN.BackColor = System.Drawing.Color.Transparent;
+            this.Login_BTN.Cursor = System.Windows.Forms.Cursors.No;
+            this.Login_BTN.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Login_BTN.Font = new System.Drawing.Font("Trebuchet MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Login_BTN.ForeColor = System.Drawing.Color.Red;
+            this.Login_BTN.Location = new System.Drawing.Point(166, 249);
+            this.Login_BTN.Name = "Login_BTN";
+            this.Login_BTN.Size = new System.Drawing.Size(127, 50);
+            this.Login_BTN.TabIndex = 5;
+            this.Login_BTN.Text = "Login";
+            this.Login_BTN.UseVisualStyleBackColor = false;
+            this.Login_BTN.Click += new System.EventHandler(this.Login_BTN_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(443, 311);
+            this.Controls.Add(this.Login_BTN);
+            this.Controls.Add(this.password_txtBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.username_txtBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Instagram Login Selenium";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox username_txtBox;
+        private System.Windows.Forms.TextBox password_txtBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Login_BTN;
+    }
+}
+
---
+++ Instagram Login Selenium/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Instagram_Login_Selenium
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+        ChromeDriver drv; Thread th;
+        // usrn =nzmslym_rec
+        string url = "https://www.instagram.com/accounts/login/";
+        private void TestNullTxtBox(object sender, EventArgs e)
+        {
+            string username = username_txtBox.Text;
+            string password= password_txtBox.Text;
+
+            if (username != "" && password != "")
+            {
+                Login_BTN.ForeColor = Color.Lime;
+                Login_BTN.Cursor = Cursors.Hand;
+            }
+            else
+            {
+                Login_BTN.ForeColor = Color.Red;
+                Login_BTN.Cursor = Cursors.No;
+            }
+        }
+        // Tested true login
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            drv = new ChromeDriver();
+            drv.Navigate().GoToUrl(url);
+        }
+
+        private void Login_BTN_Click(object sender, EventArgs e)
+        {
+            if (Login_BTN.ForeColor == Color.Lime)
+            {
+                th = new Thread(Login_Instagram); th.Start();
+            }
+        }
+
+        private void Login_Instagram()
+        {
+            try
+            {
+                drv.FindElements(By.XPath("//input[@class='_2hvTZ pexuQ zyHYP']"))[0].SendKeys(username_txtBox.Text); Thread.Sleep(2000);
+                drv.FindElement(By.XPath("//article[@class='agXmL']")).Click(); Thread.Sleep(2000);
+                // Focused Exception
+                drv.FindElements(By.XPath("//input[@class='_2hvTZ pexuQ zyHYP']"))[1].SendKeys(password_txtBox.Text); Thread.Sleep(2000);
+
+                drv.FindElement(By.XPath("//button[@class='sqdOP  L3NKy   y3zKF     ']")).Click(); Thread.Sleep(2000);
+                if(drv.Url== "https://www.instagram.com/")
+                    MessageBox.Show("Login !");
+                else
+                    MessageBox.Show("Error !");
+
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+            // button class = sqdOP  L3NKy   y3zKF     
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            drv.Quit();
+        }
+        // THANKS FOR WATCHING :)
+    }
+}
